e2b3b0d4018045e76b3007ffe31e4a3608df6c3f
Trueaminm <mohamadamineesabeigloo@Gmail.com>
2024-12-15 19:26:04 +0330

    Update BOTTOKEN.py

--- BOTTOKEN.py
+++ BOTTOKEN.py
@@ -1,4 +1,4 @@
 #you token comes here you can change it later when your first configuration deployed 
 # don't worry it works LOL
 
-TOKEN=["123456789:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"]
+TOKEN=["7713859762:AAFnnoL8r2FeYqxPTJr5dBYArJtS8-auYZA"]
